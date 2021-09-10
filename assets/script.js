260cad52c10b4ea45a00a7d9eefe693664ab8f46
Daleska <Daleskasinai99@gmail.com>
2021-09-09 21:49:36 -0300

    se agg javascrip

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var scroll = new SmoothScroll('a[href*="#primera"]', {
+	speed: 50,
+	speedAsDuration: true
+});
+var scroll = new SmoothScroll('a[href*="#segunda"]', {
+	speed: 50,
+	speedAsDuration: true
+});
+var scroll = new SmoothScroll('a[href*="#tercera"]', {
+	speed: 50,
+	speedAsDuration: true
+});
